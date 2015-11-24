bf7309c58df93d93bd8023a19ce6022c5c7321be
Perttu Pitkanen <perttu.pitka@gmail.com>
2015-11-24 16:40:27 +0900

    removed useless stuff and commented code

--- preprocess.m
+++ preprocess.m
@@ -1,23 +1,29 @@
-function [boundaries, rp] = preprocess(path)
+function preprocess(path)
     close all;
+    
     [img,map] = imread(path);
-
+    %Change color mode to rgb
     if ~isempty(map)
         img = ind2rgb(img,map);
     end
+    %Change rgb image to grayscale
     img = rgb2gray(img);
-
+    
+    %Binarization
     bwImg2=sauvola(img,[11 11],0.07);
     complementedImg = imcomplement(bwImg2);
+    %Remove blobs which areas are between the two thresholds
     %following parameters depend on the text size
-    aOpened = xor(bwareaopen(complementedImg, 80), bwareaopen(complementedImg,8000)); 
+    low = 80;
+    high = 8000;
+    aOpened = xor(bwareaopen(complementedImg, low), bwareaopen(complementedImg,high)); 
     boundaries = bwboundaries(aOpened);
 
 %     figure();
 %     subplot(2,2,1), imshow(img);
 %     subplot(2,2,2), imshow(bwImg2);
 %     subplot(2,2,3), imshow(imcomplement(aOpened)); 
-    %hold on;
+%     hold on;
     figure();
     imshow(img);
     hold on;
