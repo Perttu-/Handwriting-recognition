e7feedbaf5908d89dc908c25e41a822635876dcc
Perttu Pitkanen <perttu.pitka@gmail.com>
2015-11-25 14:25:22 +0900

    noise removal and reports

--- preprocess.m
+++ preprocess.m
@@ -2,34 +2,44 @@ function preprocess(path)
     close all;
     
     [img,map] = imread(path);
-    %Change color mode to rgb
+    
+    %Change color mode to rgb if it already isnt
     if ~isempty(map)
         img = ind2rgb(img,map);
     end
+    
     %Change rgb image to grayscale
     img = rgb2gray(img);
     
+    %Noise removal.
+    img = wiener2(img, [10, 10]); %Filter size?
+
     %Binarization
-    bwImg2=sauvola(img,[11 11],0.07);
-    complementedImg = imcomplement(bwImg2);
+    bwImg=sauvola(img,[11 11],0.07);
+    complementedImg = imcomplement(bwImg);
+    figure();
+    imshow(bwImg);
+    %imshow(xor(bwImg,bwImg2));
+    
     %Remove blobs which areas are between the two thresholds
-    %following parameters depend on the text size
-    low = 80;
+    %Following parameters depend on the text size
+    low = 60;
     high = 8000;
-    aOpened = xor(bwareaopen(complementedImg, low), bwareaopen(complementedImg,high)); 
-    boundaries = bwboundaries(aOpened);
+     aOpened = xor(bwareaopen(complementedImg, low), bwareaopen(complementedImg,high)); 
+     boundaries = bwboundaries(aOpened);
+%      boundaries = bwboundaries(complementedImg);
 
 %     figure();
 %     subplot(2,2,1), imshow(img);
 %     subplot(2,2,2), imshow(bwImg2);
 %     subplot(2,2,3), imshow(imcomplement(aOpened)); 
-%     hold on;
+
     figure();
     imshow(img);
     hold on;
     for i =1:length(boundaries)
         b = boundaries{i};
-        plot(b(:,2),b(:,1),'g','LineWidth',3);
+        plot(b(:,2),b(:,1),'g','LineWidth',1);
     end
 
 
