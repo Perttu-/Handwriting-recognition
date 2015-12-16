c858a95e8d0e3e81ae6357f665ef081563adaa25
Perttu Pitkanen <perttu.pitka@gmail.com>
2015-12-16 16:22:16 +0900

    presenting

--- preprocess.m
+++ preprocess.m
@@ -5,8 +5,8 @@ function subImages = preprocess(filename)
         p.originalImage = filename;
         p.map = filename;
         
-        p.wienerFilterSize = -1;
-        p.sauvolaNeighbourhoodSize = 130;
+        p.wienerFilterSize = 6;
+        p.sauvolaNeighbourhoodSize = 300;
         p.sauvolaThreshold = 0.2;
         p.morphOpeningLowThreshold = -1;
         p.morphOpeningHighThreshold = -1;
@@ -19,13 +19,8 @@ function subImages = preprocess(filename)
         extents = p.extents;
         solidities = p.solidities;
         
-        finalImage = p.originalImage;
+        originalImage = p.originalImage;
 
-
-        
-
-        
-        
         boundingBoxes = p.boundingBoxes;
         boundaries = p.boundaries;
         eulerNumbers = vertcat(p.eulerNumbers.EulerNumber);
@@ -40,28 +35,38 @@ function subImages = preprocess(filename)
          filter = aspectRatios < 0.06;
 %         filter = filter | aspectRatios > 7.7;
 %         filter = filter | eulerNumbers < -8;
-%         filter = filter | majorAxisLengths > 400;
-        filter = filter | areas < 16;
+         filter = filter | majorAxisLengths > 400;
+         filter = filter | areas < 16;
 %         filter = filter | areas > 5000;
 
+
+        
+        %removing the elements defined by the filters
+        %boundaries(filter)=[];
+        boundingBoxes(filter)=[];
+        
+        %imshow(finalImage);
+        lb = logical( p.finalImage );
+        st = regionprops( lb, 'Area', 'PixelIdxList' );
+        newImage = p.finalImage;
+        newImage( vertcat( st(filter).PixelIdxList ) ) = 0;
+        
         if ~1
-            properties = struct2cell(p.areas);
+            %properties = struct2cell(p.aspectRatios);
+            properties = aspectRatios;
+            newImage = 255 * uint8(newImage);
             for i = 1:length(properties)
-                finalImage = insertText(finalImage,...
+                newImage = insertText(newImage,...
                                         p.boundingBoxes(i).BoundingBox(1:2),...
-                                        num2str(properties{i}),...
+                                        num2str(properties(i)),...
                                         'BoxOpacity',0,...
                                         'FontSize',25,...
                                         'TextColor','yellow');
             end
         end
         
-        %removing the elements defined by the filters
-         %boundaries(filter)=[];
-         boundingBoxes(filter)=[];
         
-        
-        imshow(finalImage);
+        imshow(newImage);
         hold on;
         %different loops because the holes are counted as separate boundaries
         %whereas bounding boxes include the child holes
