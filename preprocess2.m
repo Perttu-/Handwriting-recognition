1e01ae6a13ce91d0f5a97823c6339fbaccf582cf
Perttu Pitkanen <perttu.pitka@gmail.com>
2016-01-15 14:28:18 +0900

    speedup the overlap process

--- preprocess2.m
+++ preprocess2.m
@@ -18,23 +18,39 @@ function preprocess2(filename)
     p.preprocess;
     toc
     
+    tic
     boundingBoxes = p.boundingBoxes;
-    %Exessive amount of combinations with large number of objects
-    pairs = nchoosek(1:length(boundingBoxes),2);
-    overlapRatios = struct('Pair', {},'OverlapRatio', {});
-
-    for i=1:length(pairs)
-        bboxA = boundingBoxes(pairs(i,1));
-        bboxB = boundingBoxes(pairs(i,2));
-        overlapRatio = bboxOverlapRatio(bboxA.BoundingBox,bboxB.BoundingBox, 'Min');
-        if overlapRatio ~= 0
-            overlapRatios(end+1) = struct('Pair',[pairs(i,1), pairs(i,2)],...
-                                           'OverlapRatio', overlapRatio);
-        end
+    bbList = zeros(length(boundingBoxes),4);
+    for i=1:length(boundingBoxes)
+        bbList(i,:)=boundingBoxes(i).BoundingBox;
     end
     
+    overlapRatio = bboxOverlapRatio(bbList,bbList);
+    width = size(overlapRatio,1);
+    %looping through the array diagonally and setting the overlap ratios of
+    %bounding boxes with itself to zero
+    overlapRatio(1:width+1:width^2) = 0;
+    toc
+    figure(1);
+    g = graph(overlapRatio); %cool
+    plot(g);
     
-    figure();
+    %Exessive amount of combinations with large number of objects
+%     pairs = nchoosek(1:length(boundingBoxes),2);
+%     overlapRatios = struct('Pair', {},'OverlapRatio', {});
+% 
+%     for i=1:length(pairs)
+%         bboxA = boundingBoxes(pairs(i,1));
+%         bboxB = boundingBoxes(pairs(i,2));
+%         overlapRatio = bboxOverlapRatio(bboxA.BoundingBox,bboxB.BoundingBox, 'Min');
+%         if overlapRatio ~= 0
+%             overlapRatios(end+1) = struct('Pair',[pairs(i,1), pairs(i,2)],...
+%                                           'OverlapRatio', overlapRatio);
+%         end
+%     end
+    
+    
+    
     newImage = p.strokeImage;
     newImage = 255 * uint8(newImage);
     
@@ -45,7 +61,7 @@ function preprocess2(filename)
         else
             property = num2str(properties(i));
         end
-        %property = i;
+        property = i;
         newImage = insertText(newImage,...
                               boundingBoxes(i).BoundingBox(1:2),...
                               property,...
@@ -53,7 +69,7 @@ function preprocess2(filename)
                               'FontSize',10,...
                               'TextColor','green');
     end
-
+    figure(2);
     imshow(newImage);
     hold on;
     for i = 1:length(boundingBoxes)
@@ -63,10 +79,9 @@ function preprocess2(filename)
                                    'EdgeColor','r',...
                                    'LineWidth',1);
     end
+    hold off;
     
 
-
-    
 %     imagesc(strokeWidthImage);
 %     colormap('jet');
 %     colorbar;
