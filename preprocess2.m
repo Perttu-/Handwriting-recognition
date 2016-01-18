e16fa32adf085a060e51e24d8909dc60c1c9294a
Perttu Pitkanen <perttu.pitka@gmail.com>
2016-01-18 16:56:26 +0900

    line detection work

--- preprocess2.m
+++ preprocess2.m
@@ -20,66 +20,81 @@ function preprocess2(filename)
     
     tic
     boundingBoxes = p.boundingBoxes;
+
+    
     bbList = zeros(length(boundingBoxes),4);
+    wideBBoxes = bbList;
+    expansionAmount = 0.09;
+    
     for i=1:length(boundingBoxes)
-        bbList(i,:)=boundingBoxes(i).BoundingBox;
+        bBox = boundingBoxes(i).BoundingBox;
+        xmin = bBox(:,1);
+        ymin = bBox(:,2);
+        xmax = xmin + bBox(:,3) - 1;
+        ymax = ymin + bBox(:,4) - 1;
+        
+        %making the boxes wider in both directions
+        xmin = (1-expansionAmount) * xmin;
+        xmax = (1+expansionAmount) * xmax;
+        
+        bbList(i,:)= [xmin,ymin,xmax,ymax];
+        wideBBoxes(i,:) = [xmin ymin xmax-xmin+1 ymax-ymin+1];
+        
     end
+    
+    
     
     overlapRatio = bboxOverlapRatio(bbList,bbList);
     width = size(overlapRatio,1);
     %looping through the array diagonally and setting the overlap ratios of
-    %bounding boxes with itself to zero
+    %bounding boxes with itself to zero (from corner to corner)
     overlapRatio(1:width+1:width^2) = 0;
     toc
+    
     figure(1);
-    g = graph(overlapRatio); %cool
+    g = graph(overlapRatio); 
     plot(g);
-    
-    %Exessive amount of combinations with large number of objects
-%     pairs = nchoosek(1:length(boundingBoxes),2);
-%     overlapRatios = struct('Pair', {},'OverlapRatio', {});
-% 
-%     for i=1:length(pairs)
-%         bboxA = boundingBoxes(pairs(i,1));
-%         bboxB = boundingBoxes(pairs(i,2));
-%         overlapRatio = bboxOverlapRatio(bboxA.BoundingBox,bboxB.BoundingBox, 'Min');
-%         if overlapRatio ~= 0
-%             overlapRatios(end+1) = struct('Pair',[pairs(i,1), pairs(i,2)],...
-%                                           'OverlapRatio', overlapRatio);
-%         end
-%     end
-    
-    
-    
+    componentIndices = conncomp(g);
+     
     newImage = p.strokeImage;
-    newImage = 255 * uint8(newImage);
+%     
+%     se = strel('line', 130, 0);
+%     lineImg = imdilate(newImage,se);
+%     lineBBoxes = regionprops(lineImg,'BoundingBox');
+%     figure();
+%     imshow(lineImg);
     
-    properties = p.strokeMetrics;
-    for i = 1:length(properties)
-        if isnan(properties(i))
-            property = 'NaN';
-        else
-            property = num2str(properties(i));
-        end
-        property = i;
-        newImage = insertText(newImage,...
-                              boundingBoxes(i).BoundingBox(1:2),...
-                              property,...
-                              'BoxOpacity',0,...
-                              'FontSize',10,...
-                              'TextColor','green');
-    end
-    figure(2);
-    imshow(newImage);
-    hold on;
-    for i = 1:length(boundingBoxes)
-        box = boundingBoxes(i).BoundingBox;
-        handles.boundingBoxes(i) = rectangle('Position',...
-                                   [box(1),box(2),box(3),box(4)],...
-                                   'EdgeColor','r',...
-                                   'LineWidth',1);
-    end
-    hold off;
+    newImage = 255 * uint8(newImage);
+    imgWideBoxes = insertShape(newImage,'Rectangle',wideBBoxes,'LineWidth',4,'Color','Green');
+    figure();
+    imshow(imgWideBoxes);
+%     properties = p.strokeMetrics;
+%     for i = 1:length(properties)
+%         if isnan(properties(i))
+%             property = 'NaN';
+%         else
+%             property = num2str(properties(i));
+%         end
+%         property = i;
+%         newImage = insertText(newImage,...
+%                               boundingBoxes(i).BoundingBox(1:2),...
+%                               property,...
+%                               'BoxOpacity',0,...
+%                               'FontSize',10,...
+%                               'TextColor','green');
+%     end
+%     figure(2);
+%     imshow(newImage);
+%     hold on;
+%     boundingBoxes = lineBBoxes;
+%     for i = 1:length(boundingBoxes)
+%         box = boundingBoxes(i).BoundingBox;
+%         handles.boundingBoxes(i) = rectangle('Position',...
+%                                    [box(1),box(2),box(3),box(4)],...
+%                                    'EdgeColor','r',...
+%                                    'LineWidth',1);
+%     end
+%     hold off;
     
 
 %     imagesc(strokeWidthImage);
