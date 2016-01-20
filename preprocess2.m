1010662f52e5765691766518be1dd76292d0f298
Perttu Pitkanen <perttu.pitka@gmail.com>
2016-01-20 16:43:34 +0900

    new testimage showed that boxes get too big

--- preprocess2.m
+++ preprocess2.m
@@ -12,7 +12,7 @@ function preprocess2(filename)
     p.morphClosingDiscSize = -1;
     %another argument to tweak
     %0.45 good for IAM database?
-    p.strokeWidthThreshold = 0.9;
+    p.strokeWidthThreshold = 100;
     
     tic
     p.preprocess;
@@ -24,31 +24,33 @@ function preprocess2(filename)
     
     bbList = zeros(length(boundingBoxes),4);
     wideBBoxes = bbList;
-    expansionAmount = 0.25;
+    expansionAmount = 0.17;
     xmins = zeros(length(boundingBoxes),1);
     xmaxs = xmins;
     ymins = xmins;
     ymaxs = xmins;
     
-    
+    %jotain vialla, laatikot paisuu liikaa
+
     for i=1:length(boundingBoxes)
         bBox = boundingBoxes(i).BoundingBox;
-        xmin = bBox(:,1);
-        ymin = bBox(:,2);
-        xmax = xmin + bBox(:,3) - 1;
-        ymax = ymin + bBox(:,4) - 1;
+        xmin = bBox(1);
+        ymin = bBox(2);
+        xmax = xmin + bBox(3) - 1;
+        ymax = ymin + bBox(4) - 1;
         
         %making the boxes wider in both directions
-%         xmin = (1-expansionAmount) * xmin;
-%         xmax = (1+expansionAmount) * xmax;
+        xmin = (1-expansionAmount) * xmin;
+        xmax = (1+expansionAmount) * xmax;
         
         
-        xmin = max(xmin, 1);
-        ymin = max(ymin, 1);
-        xmax = min(xmax, size(p.originalImage,2));
-        ymax = min(ymax, size(p.originalImage,1));
+%         xmin = max(xmin, 1);
+%         ymin = max(ymin, 1);
+%         xmax = min(xmax, size(p.originalImage,2));
+%         ymax = min(ymax, size(p.originalImage,1));
         
         bbList(i,:)= [xmin,ymin,xmax,ymax];
+       
         wideBBoxes(i,:) = [xmin ymin xmax-xmin+1 ymax-ymin+1];
         
         xmins(i) = xmin;
@@ -57,7 +59,6 @@ function preprocess2(filename)
         ymaxs(i) = ymax;
         
     end
-    
 
     
     overlapRatio = bboxOverlapRatio(wideBBoxes,wideBBoxes);
@@ -65,33 +66,31 @@ function preprocess2(filename)
     overlapRatio(1:width+1:width^2) = 0;
     toc
     
-    figure(1);
+     
     g = graph(overlapRatio); 
-    plot(g);
+%     figure(1);
+%     plot(g);
     componentIndices = conncomp(g);
     
-    xmins = accumarray(componentIndices', xmins, [], @min);
-    ymins = accumarray(componentIndices', ymins, [], @min);
-    xmaxs = accumarray(componentIndices', xmaxs, [], @max);
-    ymaxs = accumarray(componentIndices', ymaxs, [], @max);
+%     xmins = accumarray(componentIndices', xmins, [], @min);
+%     ymins = accumarray(componentIndices', ymins, [], @min);
+%     xmaxs = accumarray(componentIndices', xmaxs, [], @max);
+%     ymaxs = accumarray(componentIndices', ymaxs, [], @max);
 
-    textBBoxes = [xmins ymins xmaxs-xmins+1 ymaxs-ymins+1];
+    %textBBoxes = [xmins ymins xmaxs-xmins+1 ymaxs-ymins+1];
     
 
     %remove areas which have only one object inside them.
 
+%     histg = histcounts(componentIndices, max(unique(componentIndices)));
+%     textBBoxes(histg == 1,:)=[];
      
     newImage = p.strokeImage;
-%     
-%     se = strel('line', 130, 0);
-%     lineImg = imdilate(newImage,se);
-%     lineBBoxes = regionprops(lineImg,'BoundingBox');
-%     figure();
-%     imshow(lineImg);
-    
+         
+    %binary image to grayscale
     newImage = 255 * uint8(newImage);
-    imgWideBoxes = insertShape(newImage,'Rectangle',textBBoxes,'LineWidth',4,'Color','Green');
-    figure();
+    imgWideBoxes = insertShape(newImage,'Rectangle',textBBoxes,'LineWidth',1,'Color','Green');
+
     
     
 %     properties = p.strokeMetrics;
@@ -102,31 +101,28 @@ function preprocess2(filename)
 %             property = num2str(properties(i));
 %         end
 %         property = i;
-%         imgWideBoxes = insertText(imgWideBoxes,...
+%         newImage = insertText(newImage,...
 %                               boundingBoxes(i).BoundingBox(1:2),...
 %                               property,...
 %                               'BoxOpacity',1,...
 %                               'FontSize',10,...
 %                               'TextColor','red');
 %     end
-    imshow(imgWideBoxes);
-%     figure(2);
-%     imshow(newImage);
-%     hold on;
-%     boundingBoxes = lineBBoxes;
-%     for i = 1:length(boundingBoxes)
-%         box = boundingBoxes(i).BoundingBox;
-%         handles.boundingBoxes(i) = rectangle('Position',...
-%                                    [box(1),box(2),box(3),box(4)],...
-%                                    'EdgeColor','r',...
-%                                    'LineWidth',1);
-%     end
-%     hold off;
-    
+%     imshow(imgWideBoxes);
 
-%     imagesc(strokeWidthImage);
-%     colormap('jet');
-%     colorbar;
+    figure();
+    imshow(newImage);
+    hold on;
+    boundingBoxes = textBBoxes;
+    for i = 1:length(boundingBoxes)
+        box = boundingBoxes(i,:);
+        handles.boundingBoxes(i) = rectangle('Position',...
+                                   box,...
+                                   'EdgeColor','r',...
+                                   'LineWidth',1);
+    end
+    hold off;
+    
 
     disp(['Number of objects: ', int2str(p.objectCount)]);
     disp(['Number of rows: ', int2str(length(textBBoxes))]);
