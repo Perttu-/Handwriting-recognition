68e5f5e619f33b20b4bdbc08d2c60ffeb407532f
Perttu Pitkanen <perttu.pitka@gmail.com>
2016-03-23 16:18:35 +0900

    hough stuff

--- houghTransform.m
+++ houghTransform.m
@@ -7,10 +7,27 @@ function  houghTransform(image,thetas,rhoResolution)
     rhos = -rhoLimit:rhoResolution:rhoLimit;
     accumulatorArray = zeros(numel(rhos),numel(thetas));
     
-    for ii=rhos
-        
+    imshow(image);
+    hold on;
+    x(1)=0;
+    y(1)=0;
+    x = zeros(numel(thetas)*numel(rhos),2);
+    y = zeros(numel(thetas)*numel(rhos),2);
+    xp = x;
+    yp = y;
+
+    for t = thetas
+        for r = rhos
+            x(2)=x(1)+r*cosd(t);
+            y(2)=y(1)+r*sind(t);
+            %plot(x(2),y(2),'r*');
+            %line(x,y);
+            t2 = t+90;
+            
+
+        end
     end
-    
+
     rh = xIndices*cos(thetas)+yIndices*sin(thetas);
 
 end
