b2fc37cedef3596a78ece324a4d9cd742407763f
Perttu Pitkanen <perttu.pitka@gmail.com>
2016-04-14 18:08:32 +0900

    optimized subset1 row detection

--- src/louloudis.m
+++ src/louloudis.m
@@ -28,12 +28,12 @@ function finalBoxes = louloudis(binarizedImage)
         image = boxes(ii).Image;
         image = image+0;
         image(image==1)=ii;
-        if (0.5 * AH <= H < 3*AH) && (0.5 * AW <= W)
+        if (0.5 * AH <= H) && (H < 3*AH) && (0.5 * AW <= W)
             subset1(indx1).BoundingBox = box;
             subset1(indx1).Image = image;
             indx1 = indx1+1;
         end
-        if (H > 3 * AH)
+        if (H >= 3 * AH)
             subset2(indx2).BoundingBox = box;
             subset2(indx2).Image = image;
             indx2 = indx2+1;
@@ -47,7 +47,7 @@ function finalBoxes = louloudis(binarizedImage)
 
     %partition subset1 to equally sized boxes, extracting centroid pixels
     %and categorizing them
-    %imshow(binarizedImage);
+%     imagesc(labels);
     centroidImg = zeros(imgWidth,imgHeight);
 
     for ii = 1:length(subset1)
@@ -74,14 +74,14 @@ function finalBoxes = louloudis(binarizedImage)
             %adjust centroid and boxes to be in relation to the whole image
             %for the sake of hough transform for centroids and
             %visualization
-            %relationalBox = [newBox(1)+box(1),newBox(2)+box(2),newBox(3),newBox(4)];
+            relationalBox = [newBox(1)+box(1),newBox(2)+box(2),newBox(3),newBox(4)];
             relationalCentroid = [centroid(1)+newBox(1)+box(1)-0.5,centroid(2)+newBox(2)+box(2)-0.5];
-            %hold on;
-            %rectangle('Position',relationalBox,...
-            %'EdgeColor','y',...
-            %'LineWidth',2);
-            %plot(relationalCentroid(:,1),relationalCentroid(:,2), 'g*');
-            %hold off;
+%             hold on;
+%             rectangle('Position',relationalBox,...
+%             'EdgeColor','y',...
+%             'LineWidth',2);
+%             plot(relationalCentroid(:,1),relationalCentroid(:,2), 'g*');
+%             hold off;
             roundedCentroid = round(relationalCentroid);
             [~,~,id] = find(cropped,1);
             centroidImg(roundedCentroid(2),roundedCentroid(1))=id;
@@ -89,80 +89,58 @@ function finalBoxes = louloudis(binarizedImage)
         subset1(ii).Index = id;
     end
     
-    [accArray,thetas,rhos,voterCoordCell,voterNumberCell] = houghTransform(centroidImg,-5:5,0.2*AH);
-    tmpAcc = accArray;
+    [accArray,thetas,rhos,voterNumberCell] = houghTransform(centroidImg,-5:5,0.2*AH);
     
-    % figure();
-    % imshow(imadjust(mat2gray(accArray)),'XData',thetas,'YData',rhos,...
-    %    'InitialMagnification','fit');
-    % title('Hough Transform');
-    % xlabel('\theta'), ylabel('\rho');
-    % axis on, axis normal;
-    % colormap(hot);
+%     figure();
+%     imshow(imadjust(mat2gray(accArray)),'XData',thetas,'YData',rhos,...
+%        'InitialMagnification','fit');
+%     title('Hough Transform');
+%     xlabel('\theta'), ylabel('\rho');
+%     axis on, axis normal;
+%     colormap(hot);
     
     n1 = 5;
     n2 = 9;
 
-    textLineStruct = struct('Numbers',[]);
-    lIndex = 1;
-    newLineFlag = 1;
     
-    %this loop is taking too long, pls optimize
-    disp('loop starts')
-    
+    lineStruct = struct('Line',[]);
+    rowIndex = 1;
+    tic
     while 1
         
         sizes = cellfun('size', voterNumberCell, 1);
         [maxValue, maxIndex] = max(sizes(:));
-        [maxIRow, maxICol] = ind2sub(size(voterNumberCell),maxIndex);
-        nearVoters = voterNumberCell(maxIRow-5:maxIRow+5,maxICol);
-        voterNumbers = cell2mat(nearVoters(~cellfun('isempty',nearVoters)));
         
         if maxValue < n1
             break
         end
-        tic
-        %fix this
-%         occurences = histcounts(voterNumbers(:),[subset1(:).Index]);
-%         pieceAmounts = [subset1(:).PiecesAmount];
-%         occurences >= 0.5*pieceAmounts
-        for ii=1:length(subset1)
-            objPartsInRow=sum(voterNumbers(:) == subset1(ii).Index);
-            piecesAmount=subset1(ii).PiecesAmount;
-            if objPartsInRow >= 0.5*piecesAmount
-                objInRow = subset1(ii).Index;
-                if newLineFlag == 1
-                    oldNumbers = [];
-                else
-                    oldNumbers = textLineStruct(lIndex).Numbers;
-                end
-                
-                textLineStruct(lIndex).Numbers = [oldNumbers objInRow];
-                newLineFlag = 0;
-                %for jj=1:numel(voterNumberCell)
-                %     voters = voterNumberCell{jj};
-                %     voters(voters==objInRow)=[];
-                %     voterNumberCell{jj}=voters;
-                %end
-                removeHandle = @(x) x(x~=objInRow);
-                voterNumberCell = cellfun(removeHandle,voterNumberCell, 'UniformOutput',false);
-                
-            end
-        end
-        disp('row done')
-        toc
-        lIndex = lIndex+1;
-        newLineFlag = 1;
+        
+        [maxIRow, maxICol] = ind2sub(size(voterNumberCell),maxIndex);
+        nearVoters = voterNumberCell(maxIRow-5:maxIRow+5,maxICol);
+        voterNumbers = cell2mat(nearVoters(~cellfun('isempty',nearVoters)));
+        
+        uniqueVoters = unique(voterNumbers)';
+        pieceAmounts = [subset1.PiecesAmount];
+        pieceAmounts(~ismember([subset1.Index],uniqueVoters))=[];
+        occurences = histcounts(voterNumbers,'BinMethod','Integers');
+        occurences(occurences==0)=[];
+        
+        objsInLine = uniqueVoters(occurences >= 0.5*pieceAmounts);
+        lineStruct(rowIndex).Line = objsInLine;
+        rowIndex = rowIndex+1;
+        
+        voterNumberCell = cellfun(@(x) x(~ismember(x,objsInLine)),...
+                                  voterNumberCell,...
+                                  'UniformOutput',false);
+        
     end
-    rowLabels = labels;
     
-    % for ii = 1:length(textLineStruct)
-    %     numbers = textLineStruct(ii).Numbers;
-    %     for jj = 1:length(numbers)
-    %         number = numbers(jj);
-    %         rowLabels(rowLabels==number)=ii*100;
-    %     end
-    % end
-    % imagesc(rowLabels), colormap(jet);
+    toc
+    
+    %todo visualization of rows 
+    
+
+    
+
     finalBoxes = [];
 end
